5941d007db7d31c92fd1ad98b725f34df611a603
Issac Nicks <1975354252wangqi@gmail.com>
2022-04-18 18:02:19 +0800

    search venue with venue code

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path, include
 from .api_views import *
-from rest_framework.routers import DefaultRouter
+from rest_framework.routers import DefaultRouter, SimpleRouter
 
-router = DefaultRouter()
+router = SimpleRouter()
 router.register(r'venues', VenueViewSet, 'venue')
 router.register(r'records', RecordViewSet, 'record')
 router.register(r'members', MemberViewSet, 'member')
@@ -11,3 +11,8 @@ urlpatterns=[
     path('api/close_contacts/<int:hku_id>', get_close_contacts),
     path('api/', include(router.urls)),
 ]
+# print(router.urls)
+
+urlpatterns.extend([
+    path('api/venues/<str:venue_code>', VenueViewSet.as_view({"get":"retrieve"}))
+])
